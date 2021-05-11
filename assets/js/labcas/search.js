5a7930336a61e1275e2414a74da844e6b7445212
EDRN <edrn@mcl-labcas.jpl.nasa.gov>
2021-05-10 21:41:43 -0700

    Fixed ampersand issue for search. Removed cache of previous images in image viewer. Allow redirect in image viewer.

--- assets/js/labcas/search.js
+++ assets/js/labcas/search.js
@@ -1,16 +1,16 @@
 function search(){
     var search_text = $('#search_text').val();
     if (search_text && search_text != ""){
-	    localStorage.setItem("search", search_text);
+	    localStorage.setItem("search", search_text.replace("&","%26"));
     }else{
 	    var search_text_main = $('#search_text_main').val();
 	    if (search_text_main && search_text_main != ""){
-		localStorage.setItem("search", search_text_main);
+		localStorage.setItem("search", search_text_main.replace("&","%26"));
 		}
 	search_text = search_text_main;
     }
 
     console.log(localStorage.getItem("search"));
     console.log("Set search");
-    window.location.href = "/labcas-ui/s/index.html?search="+search_text;
+    window.location.href = "/labcas-ui/s/index.html?search="+search_text.replace("&","%26");
 }
